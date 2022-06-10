4875f53567eac8555ac88803c0ca60029c5b49c2
Lucas <luucasls2@gmail.com>
2022-06-10 10:11:58 -0400

    4o commit

--- src/data/hooks/pages/useIndex.ts
+++ src/data/hooks/pages/useIndex.ts
@@ -4,14 +4,7 @@ import { ApiService } from "../../services/ApiService"
 import { AxiosError } from "axios";
 
 export function useIndex(){
-    const [petList, setPetList] = useState<Pet[]>([
-      {
-        'id': 1,
-        'name': 'Hercules',
-        'history': 'hercules',
-        'pic': 'https://scontent.fpll5-1.fna.fbcdn.net/v/t1.6435-9/104839433_3046075485487157_2574364800378329377_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=e3f864&_nc_ohc=EvlZ7IL8i0kAX9jpnHY&tn=DedhSywW9FPpsjx5&_nc_ht=scontent.fpll5-1.fna&oh=00_AT9Cb1YgmaQhCLmthHjWDsyOgvLZ9Xfa5tGLCTK3XD3DhQ&oe=62C812B9'
-      }
-    ]),
+    const [petList, setPetList] = useState<Pet[]>([]),
         [selectedPet, setSelectedPet] = useState<Pet | null>(null),
         [email, setEmail] = useState(''),
         [value, setValue] = useState(''),
