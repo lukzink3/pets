44dca7c9cbecf554a5da3e62cefbcd2a4d406358
Lucas <luucasls2@gmail.com>
2022-06-15 21:18:08 -0400

    commit final

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const ApiService = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: process.env.NEXT_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
